85cfd58cd778067cbab87b667b385c2fd2177506
K. Pope <168753694+Bacon-Soup11@users.noreply.github.com>
2026-04-13 10:26:17 -0400

    Add files via upload

---
+++ italiexpress.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2026 at 04:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `italiexpress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `U_ID` int(5) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `D.O.B` date NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `PaymentInfo` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `Address` varchar(75) NOT NULL,
+  `TimeStamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `Order_ID` int(5) NOT NULL,
+  `U_ID` int(6) NOT NULL,
+  `Delivery_ID` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `Name` varchar(100) NOT NULL,
+  `Description` varchar(150) NOT NULL,
+  `Pricing` int(6) NOT NULL,
+  `MajorAllergen` varchar(50) NOT NULL,
+  `CalorieCount` int(8) NOT NULL,
+  `InStock` tinyint(1) NOT NULL,
+  `Image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`Name`, `Description`, `Pricing`, `MajorAllergen`, `CalorieCount`, `InStock`, `Image`) VALUES
+('Cake', 'Our special triple chocolate cake.', 8, 'Dairy, Nuts', 40, 1, 'images/cake.webp'),
+('Garlic Bread', 'Oven-baked garlic bread.', 5, 'Gluten', 14, 1, 'images/bread.jpg'),
+('Pasta', 'Homemade spaghetti noodles topped with our special meat sauce.', 10, 'Red Meat', 24, 1, 'images/pasta.jpg'),
+('Pizza', 'Pizza with fresh-sliced pepperoni, cheese, and our world renowned tomato sauce.', 11, 'Dairy, Red Meat', 32, 1, 'images/pizza.jpg'),
+('Soup', 'Creamy tomato soup.', 6, 'Dairy', 17, 1, 'image_files/soup.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_order`
+--
+
+CREATE TABLE `food_order` (
+  `Order_ID` int(5) NOT NULL,
+  `Quantity` int(75) NOT NULL,
+  `FoodName` varchar(100) NOT NULL,
+  `Total` int(10) NOT NULL,
+  `TimeStamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `isDelivery` tinyint(1) NOT NULL,
+  `U_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `food_order`
+--
+
+INSERT INTO `food_order` (`Order_ID`, `Quantity`, `FoodName`, `Total`, `TimeStamp`, `isDelivery`, `U_ID`) VALUES
+(0, 5, 'Cake', 65, '2026-04-13 13:48:42.000000', 0, 1),
+(0, 5, 'Garlic Bread', 25, '2026-04-13 13:50:49.000000', 0, 1),
+(0, 7, 'Garlic Bread', 35, '2026-04-13 13:51:05.000000', 0, 1),
+(0, 5, 'Garlic Bread', 25, '2026-04-13 14:04:19.000000', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_purchase`
+--
+
+CREATE TABLE `item_purchase` (
+  `U_ID` int(6) NOT NULL,
+  `FoodName` varchar(25) NOT NULL,
+  `TimeStamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `Quantity` int(4) NOT NULL,
+  `Total` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`U_ID`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`Delivery_ID`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`Name`);
+
+--
+-- Indexes for table `food_order`
+--
+ALTER TABLE `food_order`
+  ADD PRIMARY KEY (`Order_ID`,`U_ID`,`TimeStamp`);
+
+--
+-- Indexes for table `item_purchase`
+--
+ALTER TABLE `item_purchase`
+  ADD PRIMARY KEY (`U_ID`,`TimeStamp`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
