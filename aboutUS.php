d0796ab4e2524046ae6aa65841d3bd43d9fd94a4
K. Pope <168753694+Bacon-Soup11@users.noreply.github.com>
2026-05-01 10:01:08 -0400

    Add files via upload

---
+++ aboutUS.php
@@ -0,0 +1,69 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>About Us - Itali Express</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="icon" type="image/x-icon" href="flag.png">
+
+ <style>
+    body{
+        background-color: #E2D9C5;
+}
+</style>
+    
+</head>
+
+<body>
+
+<div class="container text-left mt-3 pb-4">
+        <h1 class="display-4 text-success" style="font-weight: bold; text-decoration: underline; font-size: 50px;">About Us</h1>
+    </div>
+
+        <div class="row px-4" style="font-family: Open Sans, Arial, sans-serif;">
+  <div class="col">
+        <p style="text-indent: 30px; font-size: 20px;">
+                At Itali Express, our focus is <b>not</b> on providing a <i>cheap</i>, Italian-<i>like</i>
+                menu but to provide the real, authentic experience curated from chefs who have 
+                trained under the best and are dedicated to bringing the customer a meal worth 
+                returning for. </p>
+                <p style="text-indent: 30px; font-size: 20px;">With ingredients sourced <i>straight</i> from the country itself
+                and dish components made in-house, we <b>guarantee</b> a fresh, <i>never</i> frozen
+                menu that provides a dining experience like <b>no other</b>.
+</p>
+               
+</div>
+  <div class="col">
+        <p style="text-indent: 30px; font-size: 20px;"> We offer classic dishes such as out hand-made spaghetti and lasagna with fresh tomato and basil, as well
+                as daily soups, wood-fire pizza, and a plethora of delicious desserts
+                crafted by out in-house baker.
+                </p>
+                <p style="text-indent: 30px; font-size: 20px;">But don't just take our word for it. Check out our review page too see 
+                        all of our very satisfied and returning customers or visit our various
+                        social media accounts for sneak peeks at upcoming menu additions and
+                        upcoming events.
+</p>
+</div>
+
+</div>
+
+<!-- footer -->
+        <footer class="text-center py-4 mt-5 border-top border-danger border-2">
+            
+            <p style="font-weight: bold; font-size: 20px; color: darkgreen;">Follow And Review Us Here: 
+                
+            <a class="mx-2 text-success" href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+            <a class="mx-2 text-success" href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
+            <a class="mx-2 text-dark" href="https://www.yelp.com/"><i class="bi bi-yelp"></i></a>
+</p>
+        </footer>
+
+        
+</body>
+</html>
